a31398a94556c9353f5d2e7d48bd9a10646c082b
Scott Mebberson <scott@scottmebberson.com>
2016-03-11 11:52:32 +1030

    Implemented the basics (more tests to come).

--- index.js
+++ index.js
@@ -1,3 +1,111 @@
+
+var url = require('url'),
+    querystring = require('querystring'),
+    crypto = require('crypto'),
+    Promise = require('bluebird'),
+    rndm = require('rndm'),
+    scmp = require('scmp'),
+    createError = require('http-errors');
+
+/**
+* Return a timestamp, optionally passing in extra seconds to add to the timestamp.
+*
+ * @param  {Number} ttl The extra seconds to add to the timestamp
+ * @return {Number}     A timestamp in seconds
+ */
+function now (ttl) {
+
+    if (ttl === undefined || ttl === null) {
+        ttl = 0;
+    }
+
+    return Math.floor(new Date()/1000) + ttl;
+
+}
+
+/**
+ * Create a HMAC digest based on a salt, a secret and a string.
+ *
+ * @param  {String} salt   The salt
+ * @param  {String} secret The secret key
+ * @param  {String} str    The string to included in the digest
+ * @return {String}        A HMAC digest
+ */
+function createDigest (salt, secret, str) {
+
+    // create the HMAC digest, using the URL path only
+    return crypto
+        .createHmac('sha256', secret)
+        .update(`${salt}.${secret}.${str}`)
+        .digest('base64');
+
+}
+
+/**
+ * Given a modified (url.query has been manipulated) URL object (from url.parse())
+ * return a string that matches what `url.path` would, but with correct values.
+ *
+ * @param  {Object} url An object as returned from `url.parse()` with a modified `query` property.
+ * @return {String}     A string of the path (i.e. url.path + url.pathname)
+ */
+function urlPath (url) {
+
+    // update the search, based on the query property
+    url.search = querystring.stringify(url.query);
+
+    // take into consideration empty search property when returning the string
+    return url.pathname + (url.search ? `?${url.search}` : '');
+
+}
+
+/**
+ * Given a modified (url.query has been manipulated) URL object (from url.parse())
+ * return a string that mtaches what `url.path` would, but with correct values.
+ *
+ * @param  {Object} url An object as returned from `url.parse()` with a modified `query` property.
+ * @return {String}     A string of the entire URL (including protocol, domain, pathname, path and search).
+ */
+function urlFormat (url) {
+
+    url.search = querystring.stringify(url.query);
+
+    return url.format();
+
+}
+
+/**
+ * Verifies the URL being actioned by Express.
+ *
+ * @param  {String} path    The path of the URL
+ * @param  {String} salt    The salt
+ * @param  {String} secret  The secret key
+ * @return {String|Boolean} true for valid, 'invalid' for 'invalid', 'timedout' if the request is too late
+ */
+function verifyUrl (path, salt, secret) {
+
+    // recreate a digest from the URL path, minus the signature
+    var parsedUrl = url.parse(path, true),
+        parsedSignature = parsedUrl.query.signature,
+        digest;
+
+    // remove the signature as that isn't part of the signed string
+    delete parsedUrl.query.signature;
+
+    // recreate the digest
+    digest = createDigest(salt, secret, urlPath(parsedUrl));
+
+    // if we don't have the same value, we're unverified
+    if (!scmp(digest, parsedSignature)) {
+        return 'invalid';
+    }
+
+    parsedUrl.query.expires = parseInt(parsedUrl.query.expires) || 0;
+
+    // verify if we're still within the expires timestamp
+    return (now() < parsedUrl.query.expires) ? true : 'timedout';
+
+}
+
 /**
  * HMAC signed URLs middleware.
  *
@@ -12,16 +120,120 @@ module.exports = function hsudb (options) {
 
     options = options || {};
 
-    return function hsudbMiddleware (req, res, next) {
+    if (!options.secret) {
+        throw Error('You must provide a secret for HSUDB to sign with.');
+    }
 
-        // lazy-load our signUrl function
-        req.signUrl = function signUrl (urlToSign) {
+    if (!options.store) {
+        throw Error('You must provide a store function for HSUDB to request a salt be stored.');
+    }
 
-            return urlToSign;
+    if (!options.retrieve) {
+        throw Error('You must provide a retrieve function for HSUDB to request a salt be retrieved.');
+    }
 
+    if (!options.complete) {
+        throw Error('You must provide a complete function for HSUDB to request a salt be removed.');
+    }
+
+    // get ttl options (default to 1 hour)
+    var ttl = parseInt(options.ttl) || 60*60;
+
+    // return a function that will scope everything to an id (so we can use this middleware multiple times)
+    return function (id) {
+
+        if (!id) {
+            throw Error('You must provide an id for HSUDB to scope with.');
         }
 
-        return next();
+        return {
+
+            setup: function hsudbSetupMiddleware (req, res, next) {
+
+                // lazy-load our signUrl function
+                req.signUrl = function signUrl (urlToSign, callback) {
+
+                    // return a promise, but use the callback if one was provided
+                    return new Promise(function (resolve, reject) {
+
+                        // create the salt and hand off to the hook to store it
+                        var salt = rndm();
+
+                        options.store(id, salt, function (err) {
+
+                            if (err) {
+                                return reject(err);
+                            }
+
+                            // with the salt stored, let's continue
+                            var parsedUrl = url.parse(urlToSign, true),
+                                expires = now(ttl),
+                                digest;
+
+                            // update the parsedUrl with the expries value before it is signed
+                            // this protects us from tampering with the value
+                            parsedUrl.query.expires = expires;
+
+                            // create the digest
+                            digest = createDigest(salt, options.secret, urlPath(parsedUrl));
+
+                            // now update the url with the information
+                            parsedUrl.query.signature = digest;
+
+                            // return the updated and signed URL
+                            return resolve(urlFormat(parsedUrl));
+
+                        });
+
+                    }).asCallback(callback);
+
+                }
+
+                return next();
+
+            },
+
+            verify: function hsudbVerifyMiddleware (req, res, next) {
+
+                // retrieve the salt from the hook
+                options.retrieve(id, function (err, salt) {
+
+                    if (err) {
+                        return next(err);
+                    }
+
+                    // a salt should always exist, try and verify the request
+                    var verified = verifyUrl(req.originalUrl, salt, options.secret);
+
+                    if (verified === 'invalid') {
+                        throw createError(403, 'invalid HMAC digest', {
+                            code: 'EBADHMACDIGEST'
+                        });
+                    }
+
+                    if (verified === 'timedout') {
+                        throw createError(403, 'URL has timed out', {
+                            code: 'ETIMEOUTHMACDIGEST'
+                        });
+                    }
+
+                    return next();
+
+                });
+
+            },
+
+            complete: function hsudbCompleteMiddleware (req, res, next) {
+
+                req.hsudbComplete = function () {
+                    options.complete(id);
+                }
+
+                return next();
+
+            }
+
+        }
 
     }
 
--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 
-var express = require('express'),
+var url = require('url'),
+    express = require('express'),
     request = require('supertest'),
     expect = require('chai').expect,
+    rndm = require('rndm'),
     hsudb = require('../');
 
 /**
@@ -29,22 +31,492 @@ function createAgent (app) {
 
 }
 
-describe('HSUDB is middleware', function () {
+describe('HSUDB', function () {
 
-    // generate our HSUDB middleware
-    var hsudbProtect = hsudb();
+    it('must be passed a secret', function () {
 
-    it('that provides a signUrl function', function (done) {
+        var fn = function () {
+            var middleware = hsudb();
+        };
 
-        var app = createApp();
+        expect(fn).to.throw(Error, /secret/);
 
-        app.get('/', hsudbProtect, function (req, res, next) {
-            return res.status(200).send(Object.keys(req).indexOf('signUrl') >= 0 && typeof req.signUrl === 'function');
+    });
+
+    it('must be passed a store function', function () {
+
+        var fn = function () {
+            var middleware = hsudb({
+                secret: 'secret'
+            });
+        };
+
+        expect(fn).to.throw(Error, /store/);
+
+    });
+
+    it('must be passed a retrieve function', function () {
+
+        var fn = function () {
+            var middleware = hsudb({
+                secret: 'secret',
+                store: function () { }
+            });
+        };
+
+        expect(fn).to.throw(Error, /retrieve/);
+
+    });
+
+    it('must be passed a complete function', function () {
+
+        var fn = function () {
+            var middleware = hsudb({
+                secret: 'secret',
+                store: function () { },
+                retrieve: function () { }
+            });
+        };
+
+        expect(fn).to.throw(Error, /complete/);
+
+    });
+
+    describe('returns a scoping function that', function () {
+
+        it('must be passed an id', function () {
+
+            var fn = function () {
+                var hsudbProtect = hsudb({
+                    secret: 'secret',
+                    store: function () { },
+                    retrieve: function () { },
+                    complete: function () { }
+                })();
+            };
+
+            expect(fn).to.throw(Error, /id/);
+
         });
 
-        createAgent(app)
-        .get('/')
-        .expect(200, 'true', done);
+        it('returns three states of middleware', function () {
+
+            var id = rndm(),
+                hsudbProtect = hsudb({
+                    secret: 'secret',
+                    store: function () { },
+                    retrieve: function () { },
+                    complete: function () { }
+                });
+
+            expect(hsudbProtect(id)).to.have.property('setup');
+            expect(hsudbProtect(id)).to.have.property('verify');
+            expect(hsudbProtect(id)).to.have.property('complete');
+
+        });
+
+        it('allows multiple instances of HSUDB to run concurrently', function (done) {
+
+            var idOne = rndm(),
+                idTwo = rndm(),
+                urlToSignOne = '/one?user=6dg3tct749fj&ion=1&espv=2',
+                urlToSignTwo = '/two?user=6dg3tct749fj&ion=1&espv=2',
+                hsudbProtectOne = hsudb({
+                    secret: 'secret',
+                    store: function (id, salt, callback) {
+
+                        expect(id).to.equal(idOne);
+                        expect(id).to.not.equal(idTwo);
+
+                    },
+                    retrieve: function () { },
+                    complete: function () { }
+                }),
+                hsudbProtectTwo = hsudb({
+                    secret: 'secret',
+                    store: function (id, salt, callback) {
+
+                        expect(id).to.equal(idTwo);
+                        expect(id).to.not.equal(idOne);
+
+                    },
+                    retrieve: function () { },
+                    complete: function () { }
+                });
+
+                var app = createApp();
+
+                app.get('/pre/one', hsudbProtectOne(idOne).setup, function (req, res, next) {
+                    req.signUrl(urlToSignOne);
+                    res.status(200).end();
+                });
+
+                app.get('/pre/two', hsudbProtectTwo(idTwo).setup, function (req, res, next) {
+                    req.signUrl(urlToSignTwo);
+                    res.status(200).end();
+                });
+
+                var agent = createAgent(app);
+
+                agent
+                .get('/pre/one')
+                .expect(200, function (errOne, res) {
+
+                    if (errOne) {
+                        return done(errOne);
+                    }
+
+                    agent
+                    .get('/pre/two')
+                    .expect(200, done);
+
+                });
+
+        });
+
+        describe('returns middleware that', function () {
+
+            it('provides a signUrl function', function (done) {
+
+                var id = rndm(),
+                    app = createApp(),
+                    hsudbProtect = hsudb({
+                        secret: 'secret',
+                        store: function () { },
+                        retrieve: function () { },
+                        complete: function () { }
+                    });
+
+                app.get('/', hsudbProtect(id).setup, function (req, res, next) {
+                    return res.status(200).send(Object.keys(req).indexOf('signUrl') >= 0 && typeof req.signUrl === 'function');
+                });
+
+                createAgent(app)
+                .get('/')
+                .expect(200, 'true', done);
+
+            });
+
+            describe('the signUrl function', function () {
+
+                it('returns a promise', function (done) {
+
+                    var id = rndm(),
+                        app = createApp(),
+                        store,
+                        hsudbProtect;
+
+                    // define the store function
+                    store = function (_id, salt, callback) {
+                        return callback();
+                    };
+
+                    // create an instance of hsudb
+                    hsudbProtect = hsudb({
+                        secret: 'secret',
+                        store: store,
+                        retrieve: function () { },
+                        complete: function () { }
+                    })
+
+                    app.get('/', hsudbProtect(id).setup, function (req, res, next) {
+
+                        var urlToSign = '/entry?user=6dg3tct749fj&ion=1&espv=2';
+
+                        req.signUrl(urlToSign).then(function (signedUrl) {
+
+                            expect(signedUrl).to.contain(urlToSign);
+
+                            return res.status(200).end();
+
+                        }, done);
+
+                    });
+
+                    createAgent(app)
+                    .get('/')
+                    .expect(200, done);
+
+                });
+
+                it('works with callbacks', function (done) {
+
+                    var id = rndm(),
+                        app = createApp(),
+                        store,
+                        hsudbProtect;
+
+                    // define the store function
+                    store = function (_id, salt, callback) {
+                        return callback();
+                    };
+
+                    // create an instance of hsudb
+                    hsudbProtect = hsudb({
+                        secret: 'secret',
+                        store: store,
+                        retrieve: function () { },
+                        complete: function () { }
+                    })
+
+                    app.get('/', hsudbProtect(id).setup, function (req, res, next) {
+
+                        var urlToSign = '/entry?user=6dg3tct749fj&ion=1&espv=2';
+
+                        req.signUrl(urlToSign, function (err, signedUrl) {
+
+                            expect(err).to.not.exist;
+                            expect(signedUrl).to.contain(urlToSign);
+
+                            return res.status(200).end();
+
+                        });
+
+                    });
+
+                    createAgent(app)
+                    .get('/')
+                    .expect(200, done);
+
+                });
+
+            });
+
+            it('will request to store a salt', function (done) {
+
+                var id = rndm(),
+                    app = createApp(),
+                    store,
+                    hsudbProtect;
+
+                // define the store function
+                store = function (_id, salt, callback) {
+                    return done();
+                };
+
+                // create an instance of hsudb
+                hsudbProtect = hsudb({
+                    secret: 'secret',
+                    store: store,
+                    retrieve: function () { },
+                    complete: function () { }
+                })
+
+                app.get('/', hsudbProtect(id).setup, function (req, res, next) {
+
+                    var urlToSign = '/entry?user=6dg3tct749fj&ion=1&espv=2';
+
+                    req.signUrl(urlToSign);
+
+                });
+
+                createAgent(app)
+                .get('/')
+                .end();
+
+            });
+
+            it('will request to retrieve a salt', function (done) {
+
+                var id = rndm(),
+                    app = createApp(),
+                    agent = createAgent(app),
+                    urlToSign = '/verify?user=6dg3tct749fj&ion=1&espv=2',
+                    signedUrl,
+                    store,
+                    retrieve,
+                    hsudbProtect;
+
+                // define the store function
+                store = function (_id, salt, callback) {
+                    return callback();
+                };
+
+                retrieve = function (_id, callback) {
+                    return done()
+                };
+
+                // create an instance of hsudb
+                hsudbProtect = hsudb({
+                    secret: 'secret',
+                    store: store,
+                    retrieve: retrieve,
+                    complete: function () { }
+                })
+
+                app.get('/', hsudbProtect(id).setup, function (req, res, next) {
+
+                    req.signUrl(urlToSign).then(function (_signedUrl) {
+                        signedUrl = _signedUrl;
+                        return res.status(200).end();
+                    }, done);
+
+                });
+
+                app.get('/verify', hsudbProtect(id).verify, function (req, res, next) {
+                    res.status(200).send('entered');
+                });
+
+                agent
+                .get('/')
+                .expect(200, function (err, res) {
+
+                    if (err) {
+                        return done(err);
+                    }
+
+                    agent
+                    .get(url.parse(signedUrl, true).path)
+                    .end();
+
+                });
+
+            });
+
+            it('will request to remove a salt', function (done) {
+
+                var id = rndm(),
+                    app = createApp(),
+                    agent = createAgent(app),
+                    urlToSign = '/verify?user=6dg3tct749fj&ion=1&espv=2',
+                    db = {},
+                    signedUrl,
+                    store,
+                    retrieve,
+                    complete,
+                    hsudbProtect;
+
+                // define the store function
+                store = function (_id, salt, callback) {
+                    db[_id] = salt;
+                    return callback();
+                };
+
+                retrieve = function (_id, callback) {
+                    return callback(null, db[_id]);
+                };
+
+                complete = function (_id, callback) {
+                    return done();
+                };
+
+                // create an instance of hsudb
+                hsudbProtect = hsudb({
+                    secret: 'secret',
+                    store: store,
+                    retrieve: retrieve,
+                    complete: complete
+                })
+
+                app.get('/', hsudbProtect(id).setup, function (req, res, next) {
+
+                    req.signUrl(urlToSign).then(function (_signedUrl) {
+                        signedUrl = _signedUrl;
+                        return res.status(200).end();
+                    }, done);
+
+                });
+
+                app.get('/verify', hsudbProtect(id).verify, function (req, res, next) {
+                    res.status(200).send('entered');
+                });
+
+                app.get('/complete', hsudbProtect(id).complete, function (req, res, next) {
+                    req.hsudbComplete();
+                    res.status('200').send('complete');
+                });
+
+                agent
+                .get('/')
+                .expect(200, function (err, res) {
+
+                    if (err) {
+                        return done(err);
+                    }
+
+                    agent
+                    .get(url.parse(signedUrl, true).path)
+                    .expect(200, 'entered', function (signedErr, signedRes) {
+
+                        if (signedErr) {
+                            return done(signedErr);
+                        }
+
+                        agent
+                        .get('/complete')
+                        .end();
+
+                    });
+
+                });
+
+            });
+
+            describe('will sign a URL', function () {
+
+                it('and verify the url', function (done) {
+
+                    var id = rndm(),
+                        app = createApp(),
+                        agent = createAgent(app),
+                        db = {},
+                        urlToSign = '/entry?user=6dg3tct749fj&ion=1&espv=2',
+                        signedUrl,
+                        store,
+                        retrieve,
+                        hsudbProtect;
+
+                    // define the store function
+                    store = function (_id, salt, callback) {
+                        db[_id] = salt;
+                        return callback();
+                    };
+
+                    // define the retrieve function
+                    retrieve = function (_id, callback) {
+                        return callback(null, db[_id]);
+                    };
+
+                    // create an instance of hsudb
+                    hsudbProtect = hsudb({
+                        secret: 'secret',
+                        store: store,
+                        retrieve: retrieve,
+                        complete: function () { }
+                    })
+
+                    app.get('/sign-url', hsudbProtect(id).setup, function (req, res, next) {
+
+                        req.signUrl(urlToSign).then(function (_signedUrl) {
+                            signedUrl = _signedUrl;
+                            return res.status(200).end();
+                        }, done);
+
+                    });
+
+                    app.get('/entry', hsudbProtect(id).verify, function (req, res, next) {
+                        res.status(200).send('entered');
+                    });
+
+                    agent
+                    .get('/sign-url')
+                    .expect(200, function (err, res) {
+
+                        if (err) {
+                            return done(err);
+                        }
+
+                        agent
+                        .get(url.parse(signedUrl, true).path)
+                        .expect(200, 'entered', done);
+
+                    });
+
+                });
+
+            });
+
+        });
 
     });
 
